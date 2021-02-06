b13a92c7db3ff14add58f1482ce379336c731b7f
Aiden Szeto <aszeto35@gmail.com>
2021-02-06 11:50:35 -0800

    new routes and drive by fixes

--- backend/userModel.js
+++ backend/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mognoose.Schema;
+const Schema = mongoose.Schema;
 
 const userModel = new Schema({
   username: { type: String, required: true },

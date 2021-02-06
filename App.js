37f0e95978ae593b08803c91fb1c4b9b2d57943f
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-02-06 00:08:43 -0800

    Setup Multiple Pages Navigation
    https://reactnative.dev/docs/navigation
    Now our App has the infrastructure to hold multiple screens. Yay!

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// React + react-native
+import * as React from "react";
+import { Button, Text } from "react-native";
 
-export default function App() {
+//Navigation
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>I made a change.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+    />
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+export default App;

6cc3ec53729a72fb402eb1df40ffe61986ab8165
PrateekS1917 <61072843+PrateekS1917@users.noreply.github.com>
2021-02-06 09:34:50 -0800

    setup backend

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./userRoute.js");
+
+require("dotenv").config;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.DB;
+const port = process.env.port || 5000;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = moongose.connection;
+connection.once("open", () => {
+  console.log("connected to mongoDB");
+});
+app.use("/", userRoute);
+app.listen(port, () => {
+  console.log(`app connected to port ${port}`);
+});
